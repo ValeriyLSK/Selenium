5b00a88c5d5487aff3ba0316a19c6dd0efd00240
ValeriyLSK <valeriyleschenko@yandex.ru>
2022-04-06 17:37:15 +0300

    add test

---
+++ src/test/java/OrderTest.java
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class OrderTest {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "/driver/windows/chromedriver.exe");
+    }
+
+    @BeforeEach
+    public void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    public void shouldSendForm() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Майкл Джордан");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79999999999");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String actualText = driver.findElement(By.cssSelector("[data-test-id='order-success'].paragraph")).getText().trim();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expected, actualText);
+    }
+}
